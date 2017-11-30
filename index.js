1174c29fd434b2f7b0a4d5be9ea3e01656fe5043
Andy Parker <andyparker84@gmail.com>
2017-11-30 14:09:14 -0600

    Server setup for testing

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const express = require("express");
 const app = express();
 
+app.get("/", (req, res) => {
+  res.send({ hi: "there!" });
+});
+
 const PORT = process.env.port || 3001;
 app.listen(PORT);
